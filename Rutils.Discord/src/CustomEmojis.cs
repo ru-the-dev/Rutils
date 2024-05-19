6fe07b79819d0addf808319bc14ce12ebe36c65d
Ruben Versavel <dev.ru.vero@gmail.com>
2024-05-19 16:55:02 +0200

    Added new custom emojis, pushed new versions to nuget

--- Rutils.Discord/src/CustomEmojis.cs
+++ Rutils.Discord/src/CustomEmojis.cs
@@ -6,8 +6,12 @@ public static class CustomEmojis
 {
     //Global == 
     public static string RedCross = "❌";
+    public static string GreenCheck = "✅";
+    public static string Info = "ℹ️";
+    public static string Warning = "⚠️";
+    
 
-    //Server1 ====
+    //Server1 = https://discord.gg/AZmkqhXJ6K
     public static Emote LoadingDots{ get => _loadingDots; }
     private static Emote _loadingDots = Emote.Parse("<a:loadingdots:1208002028442361926>");
 }
