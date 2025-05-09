db09212492857210a67ff15435a5129ba204ec18
ru-the-dev <dev.ru.vero@gmail.com>
2025-05-09 16:06:38 +0000

    Added Truncate string extention

---
+++ Rutils/src/Extentions/StringExtentions.cs
@@ -0,0 +1,21 @@
+namespace Rutils;
+
+public static class StringExtentions
+{
+    public static string FirstCharToUpper(this string s)
+    {
+        if (string.IsNullOrEmpty(s))
+            throw new ArgumentException("There is no first letter");
+
+        char[] a = s.ToCharArray();
+        a[0] = char.ToUpper(a[0]);
+        return new string(a);
+    }
+
+    public static string Truncate(this string s, int maxLength, string truncationString = "..." )
+    {
+        maxLength = Math.Clamp(maxLength - truncationString.Length, 0, s.Length);
+
+        return s.Substring(0, maxLength) + truncationString; 
+    }
+}
--- Rutils/src/StringHelpers.cs
+++ Rutils/src/StringHelpers.cs
@@ -139,14 +139,4 @@ public static class StringHelpers
         //we have a finished string table
         return tableString;
     }
-
-    public static string FirstCharToUpper(this string s)
-    {
-        if (string.IsNullOrEmpty(s))
-            throw new ArgumentException("There is no first letter");
-
-        char[] a = s.ToCharArray();
-        a[0] = char.ToUpper(a[0]);
-        return new string(a);
-    }
 }
